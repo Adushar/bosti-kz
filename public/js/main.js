dc9b5b43380ea55c4e39ca8cbee1311872859226
ANDREY DUSHAR <9vlad1@gmail.com>
2017-08-08 11:03:52 +0300

    updated the header and footer

--- public/js/main.js
+++ public/js/main.js
@@ -1,36 +1,45 @@
 fimg.onclick = function() {
+  GSVG("somename", "test", 3, 6, "#fff");
     $("ul.nav li:first-child a").addClass('active');
     $( "#simg" ).hide();
-    $('#fimg').css("padding", "0 0 0 10vw");
-    $('#main').css("text-align", "inherit");
-    $('#main').css("padding-top", "20vh");
+    $('#fimg').css({"padding-left": "10vw", "padding-right": "0"});
+    $('#main').css({"text-align": "inherit", "padding-top": "25vh", "height": "124vh"});
     $('#fsvg').css("display", "inline-block");
-    $('#fsvg').show();
-    $('#bottom_menu').show();
-    $('#main').css("height", "124vh");
+    $("#main #fsvg").addClass('animated zoomInLeft'); //Анимация для нижнего блока
+
+    TandP.onclick = function() {
+      $('#main').css("padding-top", "20vh")
+      $('#bottom_menu').show(); //видимость нижнего меню
+      $('#ssvg').show();
+      $("#main #bottom_menu").addClass('animated zoomInLeft'); //Анимация для нижнего блока
+      $("#main #ssvg").addClass('animated zoomInLeft');
+      $('#SBlock').show();
+    }
   };
 
-/* function GSVG(SVGname, SVGtarget, SVGnumber) {
+ function GSVG(SVGname, SVGtarget, SVGtargetnum, SVGnumber, color1) {
   var list = document.getElementById(SVGtarget) //Поиск div'а
   var mainSVG = document.createElement('svg')  //Создание svg оболочки
-  var SVGwidth = SVGnumber * 30
+  var SVGheight = SVGnumber * 30
+  var Fline = SVGtargetnum * 15
 
   // Присвоение свойств оболчке
-  mainSVG.id = SVGname + '-SVG'
-  mainSVG.viewbox = '0 0 30 ' + SVGwidth
-  mainSVG.setAttribute("viewBox", "0 0 30 " + SVGwidth);
+  mainSVG.id = SVGname + '-SVG';
+  mainSVG.setAttribute("viewBox", "0 0 30 " + SVGheight);
 
 list.appendChild(mainSVG)
-  list = mainSVG.id
-   var lineSVG = document.createElement('line')
-   SVGwidth =- 30
-
+  list = document.getElementById(mainSVG.id)
+  lineSVG(0, Fline, 10, Fline)
+  SVGheight -= 15
+  lineSVG(10, 15, 10, SVGheight)
    function line(x1, y1, x2, y2) {
      var lineSVG = document.createElement('line')
-     lineSVG.setAttribute("x1", StartX)
-     lineSVG.setAttribute("y1", StartY)
-     lineSVG.setAttribute("x1", StartX)
-     lineSVG.setAttribute("x1", StartX)
+     lineSVG.setAttribute("x1", x1)
+     lineSVG.setAttribute("y1", y1)
+     lineSVG.setAttribute("x2", x2)
+     lineSVG.setAttribute("y2", y2)
+     lineSVG.setAttribute("stroke-width", "1")
+     lineSVG.setAttribute("stroke", color1)
+     list.appendChild(lineSVG)
    }
 };
-*/
