2ab02546bbe7c91c02d04134e6c6a925cd1f7abc
ANDREY DUSHAR <9vlad1@gmail.com>
2017-08-15 21:56:01 +0300

    Update

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,3 @@
 class Article < ApplicationRecord
-  validates :topic,  presence: true
-  validates :img, presence: true
-  validates :description, presence: true
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170810191356) do
+ActiveRecord::Schema.define(version: 20170815155612) do
 
   create_table "articles", force: :cascade do |t|
-    t.text "topic"
-    t.text "img"
-    t.text "description"
+    t.text "html"
+    t.text "css"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
